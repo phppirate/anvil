1d0c9dc3b65501d54ad39b2ae36e4ef7fd95d984
Sam Podlogar <sam@podlogar.com>
2017-04-28 13:31:08 -0600

    Pass #1 at adding Filtering of Archived or Revoked Servers.

--- src/js/pages/servers.vue
+++ src/js/pages/servers.vue
@@ -4,24 +4,34 @@
         <div class="panel">
             <div class="panel-heading">Account #1</div>
             <ul class="list-group">
-                <router-link tag="li" :to="'/servers/' + server.id" class="list-item flex" v-for="server in servers" :key="server.id">
-                    <div class="list-item-title">{{ server.name }}</div>
+                <router-link tag="li" :to="'/servers/' + server.id" class="list-item flex" v-for="server in filteredList" :key="server.id">
+                    <div class="list-item-title"><span class="fa fa-ban" v-show="server.revoked"></span> {{ server.name }}</div>
                     <div class="small">{{ server.ip_address }}</div>
                 </router-link>
             </ul>
+            <div class="panel-body" style="border-top: 1px solid #efefef" v-if="archivedServers.length">
+                <button class="btn is-primary btn-block" @click="showingArchived = !showingArchived">{{ showingArchived ? 'Hide' : 'Show' }} Archived Servers</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        data(){
-            return {};
-        },
+        data: () => ({
+            showingArchived: false
+        }),
         props: [],
         computed: {
             servers(){
                 return this.$store.state.servers;
+            },
+            archivedServers(){
+                return this.servers.filter(server => server.revoked);
+            },
+            filteredList(){
+                if(this.showingArchived) return this.servers
+                return this.servers.filter(server => !server.revoked);
             }
         },
         methods: {}
